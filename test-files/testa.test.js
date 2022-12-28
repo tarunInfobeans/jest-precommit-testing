11579d20ccb33a1ca01719785db5d2a8a7b445a7
tarunInfobeans <tarun.rathore@infobeans.com>
2022-12-28 18:27:48 +0530

    second commit

--- test-files/testa.test.js
+++ test-files/testa.test.js
@@ -5,6 +5,7 @@ describe("another describe here", () => {
   console.log("hey there I have entered the testa file");
   test("responds to /abc", async () => {
     const res = await request(app).get("/abc");
+    console.log('adding a console statement, just to check the commit');
     console.log("checking the console statement", res.text);
     expect(typeof res.text).toBe("string");
   });
